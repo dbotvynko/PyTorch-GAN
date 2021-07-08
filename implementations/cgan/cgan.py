fba6e6a6756c9f0668312be8461f85d75a52cc0c
Daria Botvynko <darya.botvynko@gmail.com>
2021-07-08 18:02:44 +0200

    Metrics to add (RMSE, pdf, KL)

--- implementations/cgan/cgan.py
+++ implementations/cgan/cgan.py
@@ -1,3 +1,6 @@
+'''
+    Libraries
+'''
 import argparse
 import os
 import numpy as np
@@ -5,6 +8,8 @@ import math
 
 import torchvision.transforms as transforms
 from torchvision.utils import save_image
+
+from tensorboardX import SummaryWriter
 
 from torch.utils.data import DataLoader
 from torchvision import datasets
@@ -14,191 +19,268 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch
 
-os.makedirs("images", exist_ok=True)
+from torch import optim
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--n_epochs", type=int, default=200, help="number of epochs of training")
-parser.add_argument("--batch_size", type=int, default=64, help="size of the batches")
-parser.add_argument("--lr", type=float, default=0.0002, help="adam: learning rate")
-parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")
-parser.add_argument("--b2", type=float, default=0.999, help="adam: decay of first order momentum of gradient")
-parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
-parser.add_argument("--latent_dim", type=int, default=100, help="dimensionality of the latent space")
-parser.add_argument("--n_classes", type=int, default=10, help="number of classes for dataset")
-parser.add_argument("--img_size", type=int, default=32, help="size of each image dimension")
-parser.add_argument("--channels", type=int, default=1, help="number of image channels")
-parser.add_argument("--sample_interval", type=int, default=400, help="interval between image sampling")
-opt = parser.parse_args()
-print(opt)
+import random as rand
 
-img_shape = (opt.channels, opt.img_size, opt.img_size)
+import seaborn as sb
+
+import pandas as pd
+import scipy as sp
+from scipy import interpolate
+
+from matplotlib import pyplot as plt
+
+from parcels_v2 import *
+from u_v import *
+from data_pre_processing import *
+from loss_L2 import *
+from metrics import *
 
 cuda = True if torch.cuda.is_available() else False
 
 
-class Generator(nn.Module):
-    def __init__(self):
-        super(Generator, self).__init__()
+'''
+    Args parse from command line
+'''
+#from datetime import datetime, timedelta
+parser = argparse.ArgumentParser(description='Run Mercator simulation at N days')
 
-        self.label_emb = nn.Embedding(opt.n_classes, opt.n_classes)
-
-        def block(in_feat, out_feat, normalize=True):
-            layers = [nn.Linear(in_feat, out_feat)]
-            if normalize:
-                layers.append(nn.BatchNorm1d(out_feat, 0.8))
-            layers.append(nn.LeakyReLU(0.2, inplace=True))
-            return layers
-
-        self.model = nn.Sequential(
-            *block(opt.latent_dim + opt.n_classes, 128, normalize=False),
-            *block(128, 256),
-            *block(256, 512),
-            *block(512, 1024),
-            nn.Linear(1024, int(np.prod(img_shape))),
-            nn.Tanh()
-        )
-
-    def forward(self, noise, labels):
-        # Concatenate label embedding and image to produce input
-        gen_input = torch.cat((self.label_emb(labels), noise), -1)
-        img = self.model(gen_input)
-        img = img.view(img.size(0), *img_shape)
-        return img
+parser.add_argument("epochs", help="specify number of epochs", type=int)
+parser.add_argument("lr", help="sepcify learning rate", type=float)
+parser.add_argument("batch_size", help="specify number of batches", type=int)
+parser.add_argument("days", help="specify number of days", type=int)
+parser.add_argument("N", help="specify number of trajectories", type=int)
 
 
+parser.add_argument("-v", "--verbose", action="store_true", help="increase output verbosity")
+
+args = parser.parse_args()
+
+'''
+    Init parameters
+'''
+batch_size = args.batch_size
+w_entropy = 1
+w_L2 = 0
+loss = nn.BCELoss()
+N=args.N
+days=args.batch_size.days
+steps=5*days
+lr = args.lr
+num_epochs = args.epochs
+delta_t = 6 * 60 * 60
+mu = 0
+std = 1
+
+'''
+    Gaussian noise generation ####!!!! To do outside of generator class
+'''
+def gen_z(steps, mu, std):
+    mu=torch.Tensor((mu,mu)) # Mean of noise. We defined a 2d mean to be generic
+    std=torch.Tensor((std,std)) # Std of noise. We defined a 2d std to be generic
+    z=mu+std*torch.randn(steps,2) # Gaussian noise generation
+    return z
+
+'''
+    Data init
+'''
+data_train = prepare_data('data_train_1_day.csv', N)
+
+# We initialize 1d lengths of drifter for positions and velocities
+data_train_1day = np.zeros([data_train.shape[0]//steps, steps, 2])
+
+'''
+    Divide trajectories one by one
+'''
+for i in range(0, data_train.shape[0]//steps):  
+    data_train_1day[i] = data_train[i*steps:i*steps+steps]
+    #data_pos_1day[i] = data_pos[i*steps:i*steps+steps]
+
+'''
+    Init data loader, learning rate, number of epochs
+'''
+train_loader = torch.utils.data.DataLoader(data_train_1day, batch_size=batch_size, shuffle=True)
+
+'''
+    Save loss at each epoch to show it's evolution
+'''
+loss_D = num_epochs * [0.]
+loss_G = num_epochs * [0.]
+
+'''
+    Init real and fake samples labels
+'''
+real_samples_labels = torch.ones((batch_size, 1))
+generated_samples_labels = torch.zeros((batch_size, 1))
+
+'''
+    Define model
+    Discriminator - CNN structure, LeakyReLU activation function and Sigmoid at the output to transform it to the probability space
+'''
 class Discriminator(nn.Module):
     def __init__(self):
         super(Discriminator, self).__init__()
-
-        self.label_embedding = nn.Embedding(opt.n_classes, opt.n_classes)
-
         self.model = nn.Sequential(
-            nn.Linear(opt.n_classes + int(np.prod(img_shape)), 512),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Linear(512, 512),
-            nn.Dropout(0.4),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Linear(512, 512),
-            nn.Dropout(0.4),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Linear(512, 1),
-        )
+                nn.Conv2d(in_channels = 5, out_channels = 16, kernel_size = 2), # !!!! Attention now in_channels here is 5!!!!
+                nn.LeakyReLU(),
+                nn.Dropout(0.3),
+                '''nn.Conv1d(in_channels = 16, out_channels = 32, kernel_size = 1),
+                nn.LeakyReLU(),
+                nn.Dropout(0.3),
+                nn.Conv1d(in_channels = 32, out_channels = 64, kernel_size = 1),
+                nn.LeakyReLU(),
+                nn.Dropout(0.3),
+                nn.Conv1d(in_channels = 64, out_channels = 32, kernel_size = 1),
+                nn.LeakyReLU(),
+                nn.Conv1d(in_channels = 32, out_channels = 2, kernel_size = 1), 
+                nn.LeakyReLU(),
+                nn.Conv1d(in_channels = 2, out_channels = 1, kernel_size = 1), 
+                nn.LeakyReLU(),'''
+                nn.Sigmoid()
+            )
+            
+    def forward(self, inp, condition):
+        output = self.model(inp)
+        return output
 
-    def forward(self, img, labels):
-        # Concatenate label embedding and image to produce input
-        d_in = torch.cat((img.view(img.size(0), -1), self.label_embedding(labels)), -1)
-        validity = self.model(d_in)
-        return validity
+'''
+    Define model
+    Generator - LSTM (RNN) structure, each cell outputs a velocity for the next time step (delta_t = 6 hours) 
+    Input condition - GLORYS12 model velocity field at a given position (updated after time step delta_t)
+'''
+class Generator(nn.Module):
+    def __init__(self, Ndays, batch_size):
+        super(Generator, self).__init__()
+        self.batch_size = batch_size
+        # The size of the LSTM inputs is 4 : condition (u,v) and noise (z1,z2). The size of the LSTM output is 2 (u_corr,v_corr)
+        self.lstmcell = nn.LSTMCell(4, 2)
+        # The size of the linear inputs and outputs is 2 : (u_corr,v_corr)
+        self.linear = nn.Linear(2, 2)
+        self.days = Ndays
+        
 
-
-# Loss functions
-adversarial_loss = torch.nn.MSELoss()
-
-# Initialize generator and discriminator
-generator = Generator()
+    def forward(self, inp, z):
+        # We initialize the outputs
+        outputs = []
+        hx = torch.zeros(1, 2, dtype=torch.float) # Maybe it is better to initialize these values randomly. For the moment we let them as 0.
+        cx = torch.zeros(1, 2, dtype=torch.float)
+        '''generated_u = self.days * steps * [0.]
+        generated_v = self.days * steps * [0.]'''
+        generated_TS = torch.zeros((self.batch_size, steps, 2))
+        parcels_TS = torch.zeros((self.batch_size, steps, 2))
+        print('generated TS init :')
+        print(generated_TS)
+        
+        '''
+            Iterate through the batch  ###!!!! Some lines below should be moved out of the generator class
+        '''
+        for b in range(0, self.batch_size):
+            date_start = str(inp.loc[b*steps, 'date'])[:10].split('-')
+            date_end = str(inp.loc[b*steps + steps-1, 'date'])[:10].split('-')
+            start_lon =  inp.loc[b*steps, 'lon']
+            start_lat = inp.loc[b*steps, 'lat']
+            
+            # Initalize lon_step_i and lat_step_i
+            lon_step_i=np.zeros((steps+1))
+            lat_step_i=np.zeros((steps+1))
+            
+            '''
+                Iterate through day steps (5 positions, velocities etc)
+            '''    
+            for i in range(0, steps):
+                lon_step_i[i] = start_lon
+                lat_step_i[i] = start_lat
+                #Call Parcels simulation tool for batch b to get U and V at lon_0 and lat_0 (start point of CMEMS drifter) and given start date
+                lon_traj_model, lat_traj_model = parcels(int(date_start[0]), int(date_start[1]), int(date_start[2]), int(date_end[0]), int(date_end[1]), int(date_end[2]), lon_step_i[i], lat_step_i[i])
+                # We obtain the i+1 position of Parcels to later obtain the velocities at time 0
+                lon_step_i[i+1] = lon_traj_model[:, i + 1]
+                lat_step_i[i+1] = lat_traj_model[:, i + 1]
+                # We obtain the velocities at time i zero from Parcels
+                u, v = get_Ui_Vi(lon_step_i[i], lat_step_i[i], lon_step_i[i+1], lat_step_i[i+1], delta_t)
+                # We define the input (condition + Noise) of the LSTM. size 4
+                lstm_input = torch.Tensor([u[0], v[0],z[i,0], z[i,1]])
+                hx, cx = self.lstmcell(lstm_input, (hx, cx))
+                #save generated time series
+                generated_TS[b][i] = [hx.detach().numpy()[0][0], hx.detach().numpy()[0][1]]
+                #save parcels velocities to put the corresponding condition to the discriminator
+                parcels_TS[b][i] = [u, v]
+                #update lon and lat, new generated particle position
+                start_lon, start_lat = data.loc[b*5, 'lon'] + delta_t * u_corr / 1000 * (1/111),\ 
+                data.loc[b*5, 'lat'] + delta_t * v_corr / 1000 * (1/111)
+                output = self.linear(hx)
+                outputs += [output]            
+        return generated_TS, parcels_TS
+    
+#Init G and D for optimizers
 discriminator = Discriminator()
+generator = Generator(1, batch_size)
 
+#Init optimizers
+optimizer_discriminator = torch.optim.Adam(discriminator.parameters(), lr=lr)
+optimizer_generator = torch.optim.Adam(generator.parameters(), lr=lr)
+
+#Cuda
 if cuda:
     generator.cuda()
     discriminator.cuda()
-    adversarial_loss.cuda()
+    loss.cuda()
 
-# Configure data loader
-os.makedirs("../../data/mnist", exist_ok=True)
-dataloader = torch.utils.data.DataLoader(
-    datasets.MNIST(
-        "../../data/mnist",
-        train=True,
-        download=True,
-        transform=transforms.Compose(
-            [transforms.Resize(opt.img_size), transforms.ToTensor(), transforms.Normalize([0.5], [0.5])]
-        ),
-    ),
-    batch_size=opt.batch_size,
-    shuffle=True,
-)
+writer = SummaryWriter()
+    
+'''
+    Train
+'''
+for epoch in range(num_epochs):
 
-# Optimizers
-optimizer_G = torch.optim.Adam(generator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
-optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
-
-FloatTensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
-LongTensor = torch.cuda.LongTensor if cuda else torch.LongTensor
-
-
-def sample_image(n_row, batches_done):
-    """Saves a grid of generated digits ranging from 0 to n_classes"""
-    # Sample noise
-    z = Variable(FloatTensor(np.random.normal(0, 1, (n_row ** 2, opt.latent_dim))))
-    # Get labels ranging from 0 to n_classes for n rows
-    labels = np.array([num for _ in range(n_row) for num in range(n_row)])
-    labels = Variable(LongTensor(labels))
-    gen_imgs = generator(z, labels)
-    save_image(gen_imgs.data, "images/%d.png" % batches_done, nrow=n_row, normalize=True)
-
-
-# ----------
-#  Training
-# ----------
-
-for epoch in range(opt.n_epochs):
-    for i, (imgs, labels) in enumerate(dataloader):
-
-        batch_size = imgs.shape[0]
-
-        # Adversarial ground truths
-        valid = Variable(FloatTensor(batch_size, 1).fill_(1.0), requires_grad=False)
-        fake = Variable(FloatTensor(batch_size, 1).fill_(0.0), requires_grad=False)
-
-        # Configure input
-        real_imgs = Variable(imgs.type(FloatTensor))
-        labels = Variable(labels.type(LongTensor))
-
-        # -----------------
-        #  Train Generator
-        # -----------------
-
-        optimizer_G.zero_grad()
-
-        # Sample noise and labels as generator input
-        z = Variable(FloatTensor(np.random.normal(0, 1, (batch_size, opt.latent_dim))))
-        gen_labels = Variable(LongTensor(np.random.randint(0, opt.n_classes, batch_size)))
-
-        # Generate a batch of images
-        gen_imgs = generator(z, gen_labels)
-
-        # Loss measures generator's ability to fool the discriminator
-        validity = discriminator(gen_imgs, gen_labels)
-        g_loss = adversarial_loss(validity, valid)
-
-        g_loss.backward()
-        optimizer_G.step()
-
-        # ---------------------
-        #  Train Discriminator
-        # ---------------------
-
-        optimizer_D.zero_grad()
-
-        # Loss for real images
-        validity_real = discriminator(real_imgs, labels)
-        d_real_loss = adversarial_loss(validity_real, valid)
-
-        # Loss for fake images
-        validity_fake = discriminator(gen_imgs.detach(), gen_labels)
-        d_fake_loss = adversarial_loss(validity_fake, fake)
-
-        # Total discriminator loss
-        d_loss = (d_real_loss + d_fake_loss) / 2
-
-        d_loss.backward()
-        optimizer_D.step()
-
-        print(
-            "[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f]"
-            % (epoch, opt.n_epochs, i, len(dataloader), d_loss.item(), g_loss.item())
-        )
-
-        batches_done = epoch * len(dataloader) + i
-        if batches_done % opt.sample_interval == 0:
-            sample_image(n_row=10, batches_done=batches_done)
+    print('*******************************************')
+    print('Epoch '+str(epoch))
+    print('*******************************************')
+    for batch, real_samples in enumerate(train_loader):
+        inp = data_train_1day[batch*batch_size*5 : batch*batch_size*5 + batch_size*4].reset_index(drop=True)
+        #inp_u_v = inp_generated[batch*batch_size : batch*batch_size + batch_size]
+        # we use the generator de define a first set of trajectories before starting the training
+        generated_samples = generator(inp, gen_z(steps, mu, std))
+        generated_samples = torch.Tensor(generated_samples[0]).double()
+        condition_parcels_generated = torch.Tensor(generated_samples[1]).double()
+        condition_parcels_real = torch.Tensor(data_cmems.loc[batch*batch_size*5 : batch*batch_size*5 + batch_size*4, 've':'vn'].to_numpy())
+       
+        # Discriminator training
+        optimizer_discriminator.zero_grad() 
+        out_discr_real = discriminator(real_samples.float(), condition_parcels_real) 
+        D_x = out_discr_real.mean() # D_x is initialized as zero, pas initializer ou l'initializer dans la boucle n
+        print('D_x = '+str(D_x))
+        out_discr_fake = discriminator(generated_samples.float(), condition_parcels_generated)
+        D_G_z = out_discr_fake.mean()
+        print('D_G_z = '+str(D_G_z))
+        #out_discr_fake = torch.unsqueeze(torch.squeeze(out_discr_fake), dim = 1) ## Netoyer
+        loss_discriminator = -(loss(out_discr_real, real_samples_labels) + loss(out_discr_fake, generated_samples_labels))
+        loss_G_G_z = -loss(out_discr_fake, generated_samples_labels)
+        loss_discriminator.backward()
+        optimizer_discriminator.step()
+        
+        '''print('********************************')
+        print('Generator training')
+        print('********************************')'''
+        # Generator training
+        generator = Generator(1, batch_size)
+        optimizer_generator.zero_grad()
+        generated_samples = generator(inp, gen_z(steps, mu, std))
+        generated_samples = torch.Tensor(generated_samples[0]).double()
+        condition_parcels_generated = torch.Tensor(generated_samples[1]).double()
+        output_discriminator_generated = discriminator(generated_samples.float(), condition_parcels_generated)
+        loss_generator = -w_entropy * loss(output_discriminator_generated, real_samples_labels) #+ w_L2 * L2()
+        loss_generator.backward()
+        optimizer_generator.step()
+        
+    # Output value of loss function
+    if (epoch % 10 == 0):
+        metrics = metrics()
+        rmse = metrics[0]
+        print(f"Epoch: {epoch} Loss D.: {loss_discriminator} Loss D_G_z : {loss_G_G_z} D_x : {D_x} D_G_z : {D_G_z}")
+        print(f"Epoch: {epoch} Loss G.: {loss_generator}")
+        print('RMSE between v_generated and v_cmems : {rmse}')
+        loss_D[epoch] = loss_discriminator
+        loss_G[epoch] = loss_generator
+        '''plt.plot(loss_D)
+        plt.plot(loss_G)'''
